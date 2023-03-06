0f1d1170493bc9cae72ef17aed4e413c121cfbe3
siduck <siduck@tutanota.com>
2023-03-06 13:03:40 +0530

    rm dead code

--- lua/nvchad/change_theme.lua
+++ lua/nvchad/change_theme.lua
@@ -1,46 +1,4 @@
-local utils = require "nvchad"
-
-local get_example_chadrc = function()
-  local chadrc_path = require("nvchad.utils.config").custom.default_chadrc_example_path
-
-  -- store in data variable
-  local data = require("nvchad").file("r", chadrc_path)
-
-  if not data then
-    print "Error: Could not read example chadrc!"
-    return false
-  end
-
-  return data
-end
-
-local ensure_file_exists = function(file_path, default_content)
-  -- store in data variable
-  local data = utils.file("r", file_path)
-
-  -- check if data is false or nil and create a default file if it is
-  if not data then
-    utils.file("w", file_path, default_content)
-    data = utils.file("r", file_path)
-  end
-
-  -- if the file was still not created, then something went wrong
-  if not data then
-    print(
-      "Error: Could not create: "
-        .. file_path
-        .. ". Please create it manually to set a default "
-        .. "theme. Look at the documentation for more info."
-    )
-    return false
-  end
-
-  return true
-end
-
 return function(current_theme, new_theme)
-  local config = require "nvchad.utils.config"
-
   if current_theme == nil or new_theme == nil then
     print "Error: Provide current and new theme name"
     return false
@@ -48,13 +6,6 @@ return function(current_theme, new_theme)
 
   if current_theme == new_theme then
     return
-  end
-
-  local result = ensure_file_exists(config.custom.default_chadrc_path, get_example_chadrc())
-
-  if not result then
-    print "Error: Could not set a default theme. Please set it manually in your 'chadrc.lua'."
-    return false
   end
 
   -- escape characters which can be parsed as magic chars
